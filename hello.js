c1379eae3311f7d114f068fa609412f548f4a23f
mirai <kkas0807@naver.com>
2021-07-16 16:50:43 +0900

    2021-07-16

--- hello.js
+++ hello.js
@@ -1,3 +1,65 @@
+Vue.component('hello', {
+    props: ['name'],
+    template: '<p>{{message}}</p>',
+    data: function () {
+        return {
+            message: 'hello!, ' + this.name
+        }
+
+    }
+
+});
+Vue.component('add', {
+
+    template: '<button @click="add">{{count}}</button>',
+    data() {
+        return {count: 0}
+    },
+    methods: {
+        add() {
+            this.count += 1
+            this.$emit('event-add', 1);
+        }
+    }
+
+});
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        age: 20,
+        message: '',
+        user_id: 'Mirai',
+        user_grade: 'Student',
+        totalCount: 0
+    },
+    methods: {
+        updateTotal(count){
+            this.totalCount += count
+        }
+    },
+    computed: {
+        user_info: function () {
+            return this.user_id + " : " + this.user_grade
+        },
+        age_message: function () {
+
+            let str = '성인'
+            if (this.age < 19 && this.age > 12) 
+                str = '미성년자'
+            else if (this.age > 0 && this.age <= 12) 
+                str = '유아'
+            return str
+
+        }
+    },
+    components: {
+        'local-hello': {
+            template: '<p>This is Local component</p>'
+        }
+    }
+});
+
 let title = new Vue({
     el: '#title',
     data: {
---
+++ text.js
@@ -0,0 +1,62 @@
+Vue.component('comments', {
+    template: `
+    <div>
+    작성자 : <input type="text" v-model="user"> 
+    내용 : <input type="text" v-model="comment">
+    <button @click="save">저장</button>
+    <h1>출력</h1>
+    <ul>
+    <li v-for="msg in messages"> 
+    <p> {{ msg.user }} : {{ msg.comment }} </p>
+    </li>
+    </ul>
+    </div>`,
+    data() {
+        return{
+        user: null,
+        comment: null,
+        messages: []
+        }
+    },
+    methods: {
+        save() {
+
+            const message = {
+                user: this.user,
+                comment: this.comment
+            };
+
+            this
+                .messages
+                .unshift(message)
+            this.user = null
+            this.comment = null
+        }
+    }
+
+}); //전역변수
+
+// let text = new Vue({
+//     el: '#text',
+//     data: {
+//         user: null,
+//         comment: null,
+//         messages: []
+//     },
+//     methods: {
+//         save() {
+
+//             const message = {
+//                 user: this.user,
+//                 comment: this.comment
+//             };
+
+//             this
+//                 .messages
+//                 .unshift(message)
+//             this.user = null
+//             this.comment = null
+//         }
+//     }
+
+// });
